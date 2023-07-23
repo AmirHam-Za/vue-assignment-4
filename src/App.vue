32d173b7e8621e8df9bcea2c8b2402080959ff2c
AmirHam-Za <125890933+AmirHam-Za@users.noreply.github.com>
2023-07-23 23:13:11 +0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ImageCarousel from './components/ImageCarousel.vue';
+</script>
+
+<template>
+    <div>
+        <img src="/src/assets/logo-ostad.png" alt="">
+        <h1>Image Carousel Example</h1>
+        <ImageCarousel />
+    </div>
+</template>
+
+<style>
+/* Add your global styles here, or link your preferred CSS framework */
+</style>
